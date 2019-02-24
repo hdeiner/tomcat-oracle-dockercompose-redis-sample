1b97136ad74f4aa2a9cb89dd4546619930d15e98
howarddeiner <howard.deiner@deinersoft.com>
2019-02-24 17:12:45 -0500

    Make references in build_and_test.sh point to redis server to use (not just presume the current machine).  Make the reference in the oracleConfig.properties point to the name of the container that Oracle runs in (this will have to change to a DNS supported machine name when Oracle is on a different container host).

--- build_and_test.sh
+++ build_and_test.sh
@@ -3,37 +3,41 @@
 echo "Start the redis server"
 docker-compose -f compose-redis.yml -p redis-oracle-tomcat up -d
 
+# this needs to be replaced with looking at the docker logs for redis
 echo "Waiting for redis to start"
 sleep 15
 
 echo "Bring up Oracle"
 docker-compose -f compose-oracle.yml -p redis-oracle-tomcat up -d
 
+# this needs to be replaced with looking at the docker logs for oracle
 echo "Waiting for Oracle to start"
 sleep 30
 
 # there is obvious dupication in the oracle url, username, and password that should
 # be refactored - this is an example only
+# for references to things running on this machine, you will see a lot of $(hostname)
+# for references from the Tomcat container to the Oracle container, you will see oracle.  When the comtainers are on different machines, a DNS reference will be needed.
 echo "Setup configurations in redis"
 
-redis-cli set LiquibaseDriver "driver: oracle.jdbc.driver.OracleDriver"
-redis-cli set LiquibaseClasspath "classpath: lib/ojdbc8.jar"
-redis-cli set LiquibaseOracleURL "url: jdbc:oracle:thin:@"$(hostname)":1521:xe"
-redis-cli set LiquibaseOracleDatabaseUsername "username: system"
-redis-cli set LiquibaseOracleDatabasePassword "password: oracle"
+redis-cli -h $(hostname) set LiquibaseDriver "driver: oracle.jdbc.driver.OracleDriver"
+redis-cli -h $(hostname) set LiquibaseClasspath "classpath: lib/ojdbc8.jar"
+redis-cli -h $(hostname) set LiquibaseOracleURL "url: jdbc:oracle:thin:@"$(hostname)":1521:xe"
+redis-cli -h $(hostname) set LiquibaseOracleDatabaseUsername "username: system"
+redis-cli -h $(hostname) set LiquibaseOracleDatabasePassword "password: oracle"
 
-redis-cli set OracaleConfigURL "url=jdbc:oracle:thin:@"$(hostname)":1521/xe"
-redis-cli set OraclConfigUser "user=system"
-redis-cli set OracleConfigPassword "password=oracle"
+redis-cli -h $(hostname) set OracaleConfigURL "url=jdbc:oracle:thin:@"oracle":1521/xe"
+redis-cli -h $(hostname) set OraclConfigUser "user=system"
+redis-cli -h $(hostname) set OracleConfigPassword "password=oracle"
 
-redis-cli set TomcatURL "hosturl=http://"$(hostname)":8080"
+redis-cli -h $(hostname) set TomcatURL "hosturl=http://"$(hostname)":8080"
 
 echo "Build the liquibase.properties file for Liquibase to run against"
-redis-cli get LiquibaseDriver > liquibase.properties
-redis-cli get LiquibaseClasspath >> liquibase.properties
-redis-cli get LiquibaseOracleURL >> liquibase.properties
-redis-cli get LiquibaseOracleDatabaseUsername >> liquibase.properties
-redis-cli get LiquibaseOracleDatabasePassword >> liquibase.properties
+redis-cli -h $(hostname) get LiquibaseDriver > liquibase.properties
+redis-cli -h $(hostname) get LiquibaseClasspath >> liquibase.properties
+redis-cli -h $(hostname) get LiquibaseOracleURL >> liquibase.properties
+redis-cli -h $(hostname) get LiquibaseOracleDatabaseUsername >> liquibase.properties
+redis-cli -h $(hostname) get LiquibaseOracleDatabasePassword >> liquibase.properties
 
 echo "Create database schema and load sample data"
 liquibase --changeLogFile=src/main/db/changelog.xml update
@@ -42,13 +46,14 @@ echo "Build fresh war for Tomcat deployment"
 mvn clean compile war:war
 
 echo "Build the oracleConfig.properties file for the Tomcat application under test"
-redis-cli get OracaleConfigURL > oracleConfig.properties
-redis-cli get OraclConfigUser >> oracleConfig.properties
-redis-cli get OracleConfigPassword >> oracleConfig.properties
+redis-cli -h $(hostname) get OracaleConfigURL > oracleConfig.properties
+redis-cli -h $(hostname) get OraclConfigUser >> oracleConfig.properties
+redis-cli -h $(hostname) get OracleConfigPassword >> oracleConfig.properties
 
 echo "Configuring war to point to Oracle endpoint"
 cp oracleConfig.properties target/
 
+# this needs to be replaced with looking at the docker logs for tomcat
 echo "Bring up Tomcat"
 docker-compose -f compose-tomcat.yml -p redis-oracle-tomcat up -d
 
@@ -56,7 +61,7 @@ echo "Waiting for Tomcat to start"
 sleep 120
 
 echo Smoke test
-curl -s http://localhost:8080/passwordAPI/passwordDB > temp
+curl -s http://$(hostname):8080/passwordAPI/passwordDB > temp
 if grep -q "RESULT_SET" temp
 then
     echo "SMOKE TEST SUCCESS"
